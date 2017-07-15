944b99f691fbb46c7d25b044c32b75e7d4290a95
alchemistmatt <matthew.monroe@pnnl.gov>
2017-07-14 18:09:39 -0700

    Update the data package ID ranges

--- bin/Debug/StartArchiveMgr.bat
+++ bin/Debug/StartArchiveMgr.bat
@@ -1,14 +1,19 @@
+@echo off
 c:
 cd C:\DMS_Programs\DataPackage_Archive_Manager
 
 call DMSUpdateManager.exe /P:DMSUpdateManagerOptions.xml
 
-rem Upload files from data package ID 388 or higher
-rem Filtering to only trigger an upload if the data package has a file modified after 9/26/2013
-rem Files for earlier packages can be found at \\a1.emsl.pnl.gov\prismarch\DataPkgs\Public\2013
-rem You can selectively upload earlier data packages on an ad hoc basis as needed
-DataPackage_Archive_Manager.exe "388-" /D:"2013-09-26"
+echo Upload files from data package ID 388 or higher
+echo Filtering to only trigger an upload if the data package has a file modified after 9/26/2013
+echo Files for earlier packages can be found at \\a1.emsl.pnl.gov\prismarch\DataPkgs\Public\2013
+echo You can selectively upload earlier data packages on an ad hoc basis as needed
+
+@echo on
+DataPackage_Archive_Manager.exe "388-941" /D:"2015-01-01"
+@echo off
 sleep 3
 
-rem Upload files from data package ID 891 or higher, regardless of file modification date
-DataPackage_Archive_Manager.exe "891-" 
+echo Upload files from data package ID 942 or higher, regardless of file modification date
+@echo on
+DataPackage_Archive_Manager.exe "942-" 
