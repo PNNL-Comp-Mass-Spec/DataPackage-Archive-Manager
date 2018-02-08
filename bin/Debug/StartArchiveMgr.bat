781eebbedacc5f79bcf44d00fa5a9341b1d9c42c
alchemistmatt <matthew.monroe@pnnl.gov>
2018-02-07 17:15:51 -0800

    Update the path to the DMSUpdateManger in the batch file

--- bin/Debug/StartArchiveMgr.bat
+++ bin/Debug/StartArchiveMgr.bat
@@ -1,8 +1,10 @@
 @echo off
 c:
-cd C:\DMS_Programs\DataPackage_Archive_Manager
+cd C:\DMS_Programs\DMSUpdateManager
 
 call DMSUpdateManager.exe /P:DMSUpdateManagerOptions.xml
+
+cd C:\DMS_Programs\DataPackage_Archive_Manager
 
 echo Upload files from data package ID 388 or higher
 echo Filtering to only trigger an upload if the data package has a file modified after 9/26/2013
