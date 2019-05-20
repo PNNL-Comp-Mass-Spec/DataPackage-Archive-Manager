ae71f07e311f9ce97215194bdb64014446f4bb83
alchemistmatt <matthew.monroe@pnnl.gov>
2019-05-20 15:09:37 -0700

    Add batch file

---
+++ bin/Distribute_Files.bat
@@ -0,0 +1,9 @@
+@echo on
+
+xcopy Debug\DataPackage_Archive_Manager.exe \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\DataPackage_Archive_Manager /D /Y
+xcopy Debug\*.dll \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\DataPackage_Archive_Manager /D /Y
+
+xcopy Debug\DataPackage_Archive_Manager.exe \\protoapps\DMS_Programs\DataPackage_Archive_Manager /D /Y
+xcopy Debug\*.dll \\protoapps\DMS_Programs\DataPackage_Archive_Manager /D /Y
+
+pause
