5684b7f529024747072a0c44975ddf27391558e6
alchemistmatt <matthew.monroe@pnnl.gov>
2021-06-15 11:08:58 -0700

    Also distribute the .pdb file

--- bin/Distribute_Files.bat
+++ bin/Distribute_Files.bat
@@ -1,9 +1,11 @@
 @echo on
 
 xcopy Debug\DataPackage_Archive_Manager.exe \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\DataPackage_Archive_Manager /D /Y
+xcopy Debug\DataPackage_Archive_Manager.pdb \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\DataPackage_Archive_Manager /D /Y
 xcopy Debug\*.dll \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\DataPackage_Archive_Manager /D /Y
 
 xcopy Debug\DataPackage_Archive_Manager.exe \\protoapps\DMS_Programs\DataPackage_Archive_Manager /D /Y
+xcopy Debug\DataPackage_Archive_Manager.pdb \\protoapps\DMS_Programs\DataPackage_Archive_Manager /D /Y
 xcopy Debug\*.dll \\protoapps\DMS_Programs\DataPackage_Archive_Manager /D /Y
 
 pause
