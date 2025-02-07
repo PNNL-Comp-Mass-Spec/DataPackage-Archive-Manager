0e168b6f321e022fee52d48381b2591e543240f1
alchemistmatt <matthew.monroe@pnnl.gov>
2025-02-06 16:50:01 -0800

    Update distribution batch file

--- bin/Distribute_Files.bat
+++ bin/Distribute_Files.bat
@@ -1,11 +1,11 @@
 @echo on
 
-xcopy Debug\DataPackage_Archive_Manager.exe \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\DataPackage_Archive_Manager /D /Y
-xcopy Debug\DataPackage_Archive_Manager.pdb \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\DataPackage_Archive_Manager /D /Y
-xcopy Debug\*.dll \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\DataPackage_Archive_Manager /D /Y
+xcopy Debug\net8.0\DataPackage_Archive_Manager.exe \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\DataPackage_Archive_Manager /D /Y
+xcopy Debug\net8.0\DataPackage_Archive_Manager.dll \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\DataPackage_Archive_Manager /D /Y
+xcopy Debug\net8.0\*.dll \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\DataPackage_Archive_Manager /D /Y
 
-xcopy Debug\DataPackage_Archive_Manager.exe \\protoapps\DMS_Programs\DataPackage_Archive_Manager /D /Y
-xcopy Debug\DataPackage_Archive_Manager.pdb \\protoapps\DMS_Programs\DataPackage_Archive_Manager /D /Y
-xcopy Debug\*.dll \\protoapps\DMS_Programs\DataPackage_Archive_Manager /D /Y
+xcopy Debug\net8.0\DataPackage_Archive_Manager.exe \\protoapps\DMS_Programs\DataPackage_Archive_Manager /D /Y
+xcopy Debug\net8.0\DataPackage_Archive_Manager.dll \\protoapps\DMS_Programs\DataPackage_Archive_Manager /D /Y
+xcopy Debug\net8.0\*.dll \\protoapps\DMS_Programs\DataPackage_Archive_Manager /D /Y
 
 if not "%1"=="NoPause" pause
