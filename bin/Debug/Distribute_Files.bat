d25a6d9633c514bffd1f66df6d42f7c9573b3329
alchemistmatt <matthew.monroe@pnnl.gov>
2025-02-14 16:16:53 -0800

    Copy additional files to the distribution directory

--- bin/Debug/Distribute_Files.bat
+++ bin/Debug/Distribute_Files.bat
@@ -1,23 +1,32 @@
 @echo off
 
 set TargetBase=\\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\DataPackage_Archive_Manager
+set Iteration=1
+
+:Loop
 echo Copying to %TargetBase%
 
 @echo on
-xcopy net8.0\DataPackage_Archive_Manager.exe %TargetBase% /D /Y /S
-xcopy net8.0\DataPackage_Archive_Manager.dll %TargetBase% /D /Y /S
-xcopy net8.0\*.dll %TargetBase% /D /Y /S
+xcopy net8.0\DataPackage_Archive_Manager.exe                %TargetBase% /D /Y
+xcopy net8.0\DataPackage_Archive_Manager.dll                %TargetBase% /D /Y
+xcopy net8.0\DataPackage_Archive_Manager.deps.json          %TargetBase% /D /Y
+xcopy net8.0\DataPackage_Archive_Manager.pdb                %TargetBase% /D /Y
+xcopy net8.0\DataPackage_Archive_Manager.runtimeconfig.json %TargetBase% /D /Y
+xcopy ..\..\README.md                                       %TargetBase% /D /Y
+xcopy net8.0\*.dll                                          %TargetBase% /D /Y /S
+
 @echo off
+
+if %Iteration%==2 Goto Done
 
 echo.
-set TargetBase=\\protoapps\DMS_Programs\DataPackage_Archive_Manager
-echo Copying to %TargetBase%
 
-@echo on
-xcopy net8.0\DataPackage_Archive_Manager.exe %TargetBase% /D /Y /S
-xcopy net8.0\DataPackage_Archive_Manager.dll %TargetBase% /D /Y /S
-xcopy net8.0\*.dll %TargetBase% /D /Y /S
-@echo off
+set Iteration=2
+set TargetBase=\\protoapps\DMS_Programs\DataPackage_Archive_Manager
+
+goto Loop
+
+:Done
 
 echo.
 if not "%1"=="NoPause" pause
